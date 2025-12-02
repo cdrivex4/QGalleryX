1fb1bfa527d07fd74cfa685080291b705dec4178
cdrivex4 <Curtis.hoareau@googlemail.com>
2025-12-02 20:26:25 +0400

    Cleanup: Add gitignore and remove build artifacts

--- build/.qt/appSamsungGallery_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appSamsungGallery")
-set(working_dir "D:/Dev/antigravity")
-set(src_and_dest_list
-
-)
-set(timestamp_file "D:/Dev/antigravity/build/.qt/appSamsungGallery_res.txt")
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Dev/antigravity
-# Build directory: D:/Dev/antigravity/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tst_imagemodel "D:/Dev/antigravity/build/tst_imagemodel.exe")
-set_tests_properties(tst_imagemodel PROPERTIES  _BACKTRACE_TRIPLES "D:/Dev/antigravity/CMakeLists.txt;76;add_test;D:/Dev/antigravity/CMakeLists.txt;0;")
